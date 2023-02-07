2abae0533608f639703e359345b4fa54c1ff839f
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-02-07 13:35:01 -0600

    Update conexion.php

--- dashboard/bd/conexion.php
+++ dashboard/bd/conexion.php
@@ -1,16 +1,19 @@
-<?php 
-class Conexion{	  
-    public static function Conectar() {        
-        define('servidor', 'localhost');
-        define('nombre_bd', 'crud_2019');
-        define('usuario', 'crud_2019');
-        define('password', 'siTLm4sEZ8e3JLX7');					        
-        $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
-        try{
-            $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
-            return $conexion;
-        }catch (Exception $e){
-            die("El error de Conexión es: ". $e->getMessage());
-        }
-    }
-}
+<?php
+ class Conexion{
+     public static function Conectar(){
+         define('servidor', 'mysql.juan-garcia-dev.svc.cluster.local');
+         define('nombre_bd', '$MYSQL_DATABASE');
+         define('usuario', '$MYSQL_USER');
+         define('password', '$MYSQL_PASSWORD');         
+         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+         
+         try{
+            $conexion = new PDO("mysql:host=".servidor.";dbname=".nombre_bd, usuario, password, $opciones);             
+            return $conexion; 
+         }catch (Exception $e){
+             die("El error de Conexión es :".$e->getMessage());
+         }         
+     }
+     
+ }
+?>
