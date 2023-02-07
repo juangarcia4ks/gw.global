f8d02e316d7d34b4ec012bc77b1fbff9a681f383
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-02-07 13:34:07 -0600

    Add files via upload

---
+++ bd/conexion.php
@@ -0,0 +1,16 @@
+<?php 
+class Conexion{	  
+    public static function Conectar() {        
+        define('servidor', 'mysql.juan-garcia-dev.svc.cluster.local');
+        define('nombre_bd', '$MYSQL_DATABASE');
+        define('usuario', '$MYSQL_USER');
+        define('password', '$MYSQL_PASSWORD');					        
+        $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
+        try{
+            $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
+            return $conexion;
+        }catch (Exception $e){
+            die("El error de Conexión es: ". $e->getMessage());
+        }
+    }
+}
