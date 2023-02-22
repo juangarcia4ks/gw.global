29a16e8ce6beacc3ad9b8435313894ef9a699ba2
Your Name <you@example.com>
2023-02-22 11:47:05 -0600

    db commit

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 class Conexion{	  
     public static function Conectar() {        
-        define('servidor', 'mysql.juan-garcia-dev.svc.cluster.local');
-        define('nombre_bd', '$MYSQL_DATABASE');
-        define('usuario', '$MYSQL_USER');
-        define('password', '$MYSQL_PASSWORD');					        
+        define('servidor', 'mysql-csn.csn.svc.cluster.local');
+        define('nombre_bd', 'login');
+        define('usuario', 'root');
+        define('password', 'XWN3tm4kTJmXkWqb');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
